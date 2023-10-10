f586fb46d172642812640e33e6aa42c11f2de717
Vidushi Gandhi <curiousvidushi27@gmail.com>
2023-10-10 23:00:49 +0530

    created array folder (#3)

---
+++ Array/First_and_last_occurrences_of_x.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class GFG
+{
+    ArrayList<Integer> find(int arr[], int n, int x)
+    {
+        // code here
+        ArrayList<Integer> olist = new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i] == x)
+            {
+                olist.add(i);
+                break;
+            }
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            if(arr[i] == x)
+            {
+                olist.add(i);
+                break;
+            }
+        }
+        
+        if(olist.size() != 2)
+        {
+            olist.add(-1);
+            olist.add(-1);
+        }
+        
+        return olist;
+    }
+    
+}
+
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+//            int n = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] q = line.trim().split("\\s+");
+            int n =Integer.parseInt(q[0]);
+            int x =Integer.parseInt(q[1]);
+//            //int y =Integer.parseInt(q[2]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(a1[i]);
+            }
+            GFG ob = new GFG();
+            ArrayList<Integer> ans=ob.find(arr,n,x);
+            System.out.println(ans.get(0)+" "+ans.get(1));
+        }
+    }
+}
+
+// } Driver Code Ends

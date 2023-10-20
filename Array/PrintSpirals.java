2d4dc9cdcc0ebbcf08c6250a9e02b99cf2902408
Nikhil <nikhildixit2125@gmail.com>
2023-10-21 02:13:42 +0530

    Spiral Traversal of Matrix

---
+++ Array/PrintSpirals.java
@@ -0,0 +1,76 @@
+package Array.Medium;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PrintSpirals {
+    // Time Complexity: O(m x n)
+    // Space Complexity: O(n)
+    public static List<Integer> printSpiral(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int top = 0;
+        int left = 0;
+        int bottom = n - 1;
+        int right = m - 1;
+
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        System.out.print("Enter the number of columns: ");
+        int cols = scanner.nextInt();
+
+        int[][] matrix = new int[rows][cols];
+
+        System.out.println("Enter the elements of the matrix:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        List<Integer> spiralList = PrintSpirals.printSpiral(matrix);
+
+        System.out.println("Spiral order traversal of the matrix:");
+        for (int num : spiralList) {
+            System.out.print(num + " ");
+        }
+        scanner.close();
+    }
+}
